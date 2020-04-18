9eae531886156f3ede5236afd8e288fcf7d0d0a7
wxjiyc <1729746258@qq.com>
2020-04-18 20:02:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ case $ch1 in
             echo '    HTTP_Status_Code=$(curl -I -m 10 -o /dev/null -s -w %{http_code} www.baidu.com)' >>autologin.sh
             echo '    if [ ${HTTP_Status_Code} != 200 ]; then' >>autologin.sh
             echo '        local_ip=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v 192.168|grep -v inet6|awk '"'"'{print $2}'"'"'|tr -d "addr:")' >>autologin.sh
-            echo '        curl "http://210.29.79.141:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C0%2C$1%40$3&user_password=$2&wlan_user_ip=$local_ip&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=ME60&jsVersion=3.3.2&v=2005"' >>autologin.sh
+            echo '        curl "http://210.29.79.141:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C0%2C$1%40$3&user_password=$2&wlan_user_ip=$local_ip&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.2&v=2005"' >>autologin.sh
             echo '    else' >>autologin.sh
             echo '        sleep $4' >>autologin.sh
             echo '    fi' >>autologin.sh
